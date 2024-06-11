33a3faee0ba3dc57d282e4aa374be78f056fb40c
MinboyKim <kdm3801@gmail.com>
2024-06-11 17:48:34 +0900

    add: dialog
    dialog 추가

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -4,12 +4,6 @@ export default function Header() {
   return (
     <div className="w-full h-16 p-4  border-gray-100 shadow-lg flex items-center justify-between">
       <h1 className="text-primary font-extrabold text-xl">HYPERDESK</h1>
-      <Link
-        href="/login"
-        className="text-primary p-2 hover:bg-primary-hover rounded-sm font-medium"
-      >
-        로그인
-      </Link>
     </div>
   );
 }
