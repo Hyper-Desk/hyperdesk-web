9387d8805a93d3c3cf01afa6e73780391892cbbd
MinboyKim <kdm3801@gmail.com>
2024-07-01 21:41:02 +0900

    feat: Logout
    로그아웃 로직 구현

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,6 +1,30 @@
+import { auth, signOut } from "@/auth";
 import Link from "next/link";
+import { Button } from "../ui/button";
 
-export default function Header() {
+export default async function Header() {
+  const session = await auth();
+
+  const handleLogout = async () => {
+    await signOut();
+  };
+
+  if (session?.user) {
+    return (
+      <div className="flex h-16 w-full items-center justify-between border-gray-100 p-4 shadow-lg">
+        <Link href="/" className="text-xl font-extrabold text-primary">
+          HYPERDESK
+        </Link>
+        <Button
+          onClick={handleLogout}
+          className="text-primary hover:text-primary-dark bg-white hover:bg-white"
+        >
+          로그아웃
+        </Button>
+      </div>
+    );
+  }
+
   return (
     <div className="flex h-16 w-full items-center justify-between border-gray-100 p-4 shadow-lg">
       <Link href="/" className="text-xl font-extrabold text-primary">
