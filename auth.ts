94238c334415735fbf65a71b6b151ff00f220db1
MinboyKim <kdm3801@gmail.com>
2024-07-01 17:29:37 +0900

    feat: auth
    next-auth 설정

---
+++ auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { BASE_URL } from "./lib/constant";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/login",
+    newUser: "/signup",
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        username: { label: "Username" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const authResponse = await fetch(`${BASE_URL}/user/login`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            userId: credentials.username,
+            password: credentials.password,
+          }),
+        });
+
+        if (!authResponse.ok) return null;
+
+        const user = await authResponse.json();
+
+        return user;
+      },
+    }),
+  ],
+});
