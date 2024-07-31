c0f81ce8ea234006c98d607b3243594d71cc9d91
MinboyKim <kdm3801@gmail.com>
2024-08-01 00:49:03 +0900

    add: shadcn toast
    toast 컴포넌트 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import AuthSession from "@/components/AuthSession";
+import { Toaster } from "@/components/ui/toaster";
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -22,6 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={fontSans.className}>
         <AuthSession>{children}</AuthSession>
+        <Toaster />
       </body>
     </html>
   );
