af2d4b100e49e33c081151ae2abe70c0f71e44b4
MinboyKim <kdm3801@gmail.com>
2024-08-22 02:23:41 +0900

    feat: routes error handling
    서비스 준비중 타이포, 404 처리

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -5,7 +5,7 @@ import Profile from "@/pages/profile/Profile";
 import SignUp from "@/pages/sign-up/SignUp";
 import MainLayout from "@/widgets/main-layout/MainLayout";
 import SidebarLayout from "@/widgets/sidebar-layout/SidebarLayout";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 const routes = (isLoggedIn: boolean) => [
   {
@@ -44,13 +44,39 @@ const routes = (isLoggedIn: boolean) => [
       },
       {
         path: "storage",
-        element: <div>Storage</div>,
+        element: (
+          <div className="w-full h-full flex justify-center items-center">
+            <h1 className="text-5xl font-bold text-primary">
+              서비스 준비중입니다.
+            </h1>
+          </div>
+        ),
       },
       {
         path: "firewall",
-        element: <div>Firewall</div>,
+        element: (
+          <div className="w-full h-full flex justify-center items-center">
+            <h1 className="text-5xl font-bold text-primary">
+              서비스 준비중입니다.
+            </h1>
+          </div>
+        ),
       },
     ],
+  },
+  {
+    path: "*",
+    element: (
+      <div className="w-full h-full flex flex-col gap-10 justify-center items-center">
+        <h1 className="text-5xl font-bold text-primary">404 Not Found</h1>
+        <Link
+          to="/"
+          className="text-2xl font-bold text-primary hover:underline"
+        >
+          홈으로 돌아가기
+        </Link>
+      </div>
+    ),
   },
 ];
 
